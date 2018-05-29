8a357e56510ea477dc5835f17654d8d728360e05
zhe <1655604975@qq.com>
2018-05-29 13:52:13 +0800

    '展示一些值'

--- A_Simple_Web_Server/server.py
+++ A_Simple_Web_Server/server.py
@@ -10,19 +10,41 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class RequestHandler(BaseHTTPRequestHandler):
 
     Page = '''
-    <html>
-    <body>
-    <p>Hello, web!</p>
-    </body>
-    </html>
+        <html>
+        <body>
+        <table>
+        <tr>  <td>Header</td>         <td>Value</td>          </tr>
+        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+        <tr>  <td>Client port</td>    <td>{client_port}</td>  </tr>
+        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+        </table>
+        </body>
+        </html>
     '''
 
     def do_GET(self):
+        self.create_page()
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(len(self.Page)))
+        self.send_header('Content-Length', str(len(self.page)))
         self.end_headers()
-        self.wfile.write(self.Page.encode())
+
+        self.wfile.write(self.page.encode())
+
+    def create_page(self):
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port': self.client_address[1],
+            'command': self.command,
+            'path': self.path,
+        }
+        print(values)
+        self.page = self.Page.format(**values)
+        return self.page
+
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)

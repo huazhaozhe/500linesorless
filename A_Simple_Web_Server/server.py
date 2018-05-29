2c3857cf4e304cbe09ad98d0681d999a60dbbd5e
zhe <1655604975@qq.com>
2018-05-29 15:16:05 +0800

    '静态页面'

--- A_Simple_Web_Server/server.py
+++ A_Simple_Web_Server/server.py
@@ -6,6 +6,11 @@
 # @Project  : PyCharm
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+class ServerException(Exception):
+    pass
+
 
 class RequestHandler(BaseHTTPRequestHandler):
 
@@ -24,14 +29,55 @@ class RequestHandler(BaseHTTPRequestHandler):
         </html>
     '''
 
-    def do_GET(self):
-        self.create_page()
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', str(len(self.page)))
-        self.end_headers()
+    Error_Page = """\
+        <html>
+        <body>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+        """
 
-        self.wfile.write(self.page.encode())
+    def do_GET(self):
+
+        if self.path == '/':
+            self.handle_home()
+
+        try:
+            full_path = os.getcwd() + self.path
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_home(self):
+        content = self.create_page()
+        self.send_content(content)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'r') as reader:
+                content = reader.read()
+                print(content)
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read:{1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Length', str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode())
 
     def create_page(self):
         values = {

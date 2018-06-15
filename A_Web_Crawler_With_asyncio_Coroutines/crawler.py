2714335547503243aae004157cd1c999cee17280
zhe <1655604975@qq.com>
2018-06-15 14:39:38 +0800

    '循环等待请求和响应'

--- A_Web_Crawler_With_asyncio_Coroutines/crawler.py
+++ A_Web_Crawler_With_asyncio_Coroutines/crawler.py
@@ -13,14 +13,31 @@ import time
 
 def fetch(url):
     sock = socket.socket()
-    sock.connect(('xkcd.com', 80))
+    sock.setblocking(False)
+    try:
+        sock.connect(('xkcd.com', 80))
+    except BlockingIOError:
+        pass
     request = 'GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(url)
-    sock.send(request.encode())
+    while True:
+        try:
+            sock.send(request.encode())
+            print('sent')
+            break
+        except OSError:
+            pass
+
     response = b''
-    chunk = sock.recv(4096)
-    while chunk:
-        response += chunk
-        chunk = sock.recv(4096)
+    # chunk = sock.recv(4096)
+    while True:
+        try:
+            chunk = sock.recv(4096)
+            if chunk:
+                response += chunk
+            else:
+                break
+        except BlockingIOError:
+            pass
 
     # links = parse_links(response)
     # q.add(links)
